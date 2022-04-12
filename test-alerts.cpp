df113b28a79399243154137b56c744fac1cea29c
Sowmbika Palanisamy <sowmbika@gmail.com>
2022-04-12 18:27:25 +0200

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -120,3 +120,214 @@ TEST_CASE("ValidateRange -Negative scenario- On passing 2 input values with (var
   ValidateRangeStatus = ValidateRange (-1,-5);
   REQUIRE(ValidateRangeStatus  == false);
 }
+
+TEST_CASE("AlertBreach -Positive scenario- Alert message to controller with breach level NORMAL ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_CONTROLLER,NORMAL);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 1);
+  REQUIRE(Test_PrintToEmailStubCount  == 0);   
+}
+
+
+TEST_CASE("AlertBreach -Positive scenario- Alert message to controller with breach level TOO_LOW ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_CONTROLLER,TOO_LOW);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 2); 
+  REQUIRE(Test_PrintToEmailStubCount  == 0);  
+}
+
+
+TEST_CASE("AlertBreach -Positive scenario- Alert message to controller with breach level TOO_HIGH ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_CONTROLLER,TOO_HIGH);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 3);  
+  REQUIRE(Test_PrintToEmailStubCount  == 0);  
+}
+
+//NOTE : Since for NORMAL scenario Email notification is configured as not required, Test_PrintToEmailStubCount is expected to remain 0 though
+// AlertBreachStatus is SUCCESS
+TEST_CASE("AlertBreach -Positive scenario- Alert message through Email with breach level NORMAL ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_EMAIL,NORMAL);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 3); 
+  REQUIRE(Test_PrintToEmailStubCount  == 0);  
+}
+
+
+TEST_CASE("AlertBreach -Positive scenario- Alert message through Email with breach level TOO_LOW ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_EMAIL,TOO_LOW);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 3); 
+  REQUIRE(Test_PrintToEmailStubCount  == 1);  
+}
+
+
+TEST_CASE("AlertBreach -Positive scenario- Alert message through Email with breach level TOO_HIGH ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (TO_EMAIL,TOO_HIGH);
+  REQUIRE(AlertBreachStatus  == SUCCESS);
+  REQUIRE(Test_PrintToControllerStubCount  == 3); 
+  REQUIRE(Test_PrintToEmailStubCount  == 2);  
+}
+
+TEST_CASE("AlertBreach -Negative scenario- Alert message through invalid target with breach level NORMAL ") {
+  bool AlertBreachStatus;
+  AlertBreachStatus = AlertBreach (INVALID_ALERT_TARGET,NORMAL);
+  REQUIRE(AlertBreachStatus  == FAILURE);
+  REQUIRE(Test_PrintToControllerStubCount  == 3); 
+  REQUIRE(Test_PrintToEmailStubCount  == 2);  
+}
+
+
+TEST_CASE("SendAlertToController - With various possible breach types  ") {
+  SendAlertToController (NORMAL);
+  REQUIRE(Test_PrintToControllerStubCount  == 4); 
+  REQUIRE(Test_PrintToEmailStubCount  == 2);  
+  SendAlertToController (TOO_LOW);
+  REQUIRE(Test_PrintToControllerStubCount  == 5); 
+  REQUIRE(Test_PrintToEmailStubCount  == 2);
+  SendAlertToController (TOO_HIGH);
+  REQUIRE(Test_PrintToControllerStubCount  == 6); 
+  REQUIRE(Test_PrintToEmailStubCount  == 2);
+}
+
+TEST_CASE("SendAlertToEmail - With Mail Notification Required - In case of breach level TOO_LOW and TOO_HIGH") {
+  SendAlertToEmail (TOO_LOW);
+  REQUIRE(Test_PrintToControllerStubCount  == 6); 
+  REQUIRE(Test_PrintToEmailStubCount  == 3);  
+  SendAlertToEmail (TOO_HIGH);
+  REQUIRE(Test_PrintToControllerStubCount  == 6); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4);
+}
+
+TEST_CASE("SendAlertToEmail - With Mail Notification Not Required - In case of breach level NORMAL") {
+  SendAlertToEmail (NORMAL);
+  REQUIRE(Test_PrintToControllerStubCount  == 6); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4);  
+}
+
+TEST_CASE("ValidateBattery -Positive Scenario- TO_CONTROLLER , PASSIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,PASSIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 7); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,PASSIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 8); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,PASSIVE_COOLING,45);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+ REQUIRE(Test_PrintToControllerStubCount  == 9); 
+ REQUIRE(Test_PrintToEmailStubCount  == 4); 
+}
+
+TEST_CASE("ValidateBattery -Positive Scenario- TO_CONTROLLER , HI_ACTIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,HI_ACTIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 10); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,HI_ACTIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 11); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,HI_ACTIVE_COOLING,50);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 12); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+}
+
+TEST_CASE("ValidateBattery -Positive Scenario- TO_CONTROLLER , MED_ACTIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,MED_ACTIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 13); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,MED_ACTIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 14); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_CONTROLLER,MED_ACTIVE_COOLING,45);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+}
+
+//NOTE : Since for NORMAL scenario Email notification is configured as not required, Test_PrintToEmailStubCount is expected to remain 0 though
+// ValidateBatteryStatus is SUCCESS
+TEST_CASE("ValidateBattery -Positive Scenario- TO_EMAIL , PASSIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,PASSIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 4); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,PASSIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 5); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,PASSIVE_COOLING,45);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 6); 
+}
+
+//NOTE : Since for NORMAL scenario Email notification is configured as not required, Test_PrintToEmailStubCount is expected to remain 0 though
+// ValidateBatteryStatus is SUCCESS
+TEST_CASE("ValidateBattery -Positive Scenario- TO_EMAIL , HI_ACTIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,HI_ACTIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 6); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,HI_ACTIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 7); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,HI_ACTIVE_COOLING,50);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 8); 
+}
+
+//NOTE : Since for NORMAL scenario Email notification is configured as not required, Test_PrintToEmailStubCount is expected to remain 0 though
+// ValidateBatteryStatus is SUCCESS
+TEST_CASE("ValidateBattery -Positive Scenario- TO_EMAIL , MED_ACTIVE_COOLING with various temperature values ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,MED_ACTIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 8); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,MED_ACTIVE_COOLING,-10);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 9); 
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,MED_ACTIVE_COOLING,45);
+  REQUIRE(ValidateBatteryStatus  == SUCCESS); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 10); 
+}
+
+TEST_CASE("ValidateBattery -Negative Scenario- INVALID_COOLING_TYPE ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (TO_EMAIL,INVALID_COOLING_TYPE,25);
+  REQUIRE(ValidateBatteryStatus  == FAILURE); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 10); 
+}
+
+
+TEST_CASE("ValidateBattery -Negative Scenario- INVALID_ALERT_TARGET  ") {
+  bool ValidateBatteryStatus;
+  ValidateBatteryStatus = ValidateBattery (INVALID_ALERT_TARGET ,MED_ACTIVE_COOLING,25);
+  REQUIRE(ValidateBatteryStatus  == FAILURE); 
+  REQUIRE(Test_PrintToControllerStubCount  == 15); 
+  REQUIRE(Test_PrintToEmailStubCount  == 10); 
+}
+
+
